32b8d68b23511ef94ab680233e55af738ecb0ae3
tienphuckx <tienphuckx@gmail.com>
2024-10-23 13:09:42 +0700

    init structure

---
+++ cmd/server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Initializing server...")
+}
+
+func StartServer() {
+	fmt.Println("Starting server...")
+}
+
+func StopServer() {
+	fmt.Println("Stopping server...")
+}
